1241992705683f47931886bc99f487121e178866
ReneF <rer007_@unitec.edu>
2015-03-27 14:49:49 -0600

    +main

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ using namespace std;
 
 int main(int argc, char * argv[]){
 	int op;
+	vector<Cliente*> clients;
 	do{
 		cout << "1.- Agregar Cliente" << endl
 			 << "2.- Agregar Producto" << endl
@@ -21,13 +22,23 @@ int main(int argc, char * argv[]){
 
 		switch(op){
 			case 1:{//AGREGAR CLIENTES
+				string nombre, apellido;
+				cout << "1. -Agregar cliente" << endl
+					 << "Ingrese nombre del cliente: " << endl;
+				cin >> nombre;
+				cout << "Ingrese apellido del cliente";
+				cin >> apellido;
+
+				clients.push_back(new Cliente(nombre, apellido));
+				//delete client;
+				cout << (clients.at(0)->toString());
 
 			}break;
 			case 2:{
 				int op1;
 				cout << "1. Educativo"<<endl
 					 << "2. Alcoholico"<<endl
-					 <<"3. Lujo"<<endl<<
+					 <<"3. Lujo"<<endl
 					 <<"4.Salir"<<endl;
 				cin >> op1;
 				switch(op1){
@@ -35,7 +46,20 @@ int main(int argc, char * argv[]){
 
 					}break;//FIN EDUCATIVO
 					case 2:{//ALCOHOLICO
-
+						double volumen,peso,precio;
+						int este; 
+						cout << "Ingrese el volumen del producto alcoholico"<<endl; 
+						cin >> volumen; 
+						cout <<"Ingrese el peso del producto alcoholico"<<endl; 
+						cin >> peso; 
+						cout <<"Ingrese el precio del producto alcoholico"<<endl; 
+						cin >> precio; 
+						cout << "------CLIENTE------"<<endl; 
+						for(int i=0;i < clients.size();i++){ 
+							cout << i << ") " <<clients.at(i)->toString()<<endl; 
+						} 
+						cout << "Ingrese el cliente que compro el producto."<<endl; 
+						cin >> este;
 					}break;//FIN ALCOHOLICO
 					case 3:{//LUJO
 
