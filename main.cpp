a02dbe4fb3dcfffabbfbee4e63115257b9aea4ec
Dany Cheong <danycheong@unitec.edu>
2015-03-27 14:34:53 -0600

    Agregar algo en main, para trabajar

--- main.cpp
+++ main.cpp
@@ -25,7 +25,27 @@ int main(int argc, char * argv[]){
 
 			}break;
 			case 2:{
-				
+				int op1;
+				cout << "1. Educativo"<<endl
+					 << "2. Alcoholico"<<endl
+					 <<"3. Lujo"<<endl<<
+					 <<"4.Salir"<<endl;
+				cin >> op1;
+				switch(op1){
+					case 1:{//Educativo
+
+					}break;//FIN EDUCATIVO
+					case 2:{//ALCOHOLICO
+
+					}break;//FIN ALCOHOLICO
+					case 3:{//LUJO
+
+					}break;//FIN LUJO
+					case 4:{//SALIR
+
+					}break;//FIN SALIR
+				}//FIN SWITCH PRODUCTO
+
 			}break;
 			case 3:{
 				
